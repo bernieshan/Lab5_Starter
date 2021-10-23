2f6dc6b730a55aa60c3461af2098a14169d1f6b9
bernieshan <bshan@ucsd.edu>
2021-10-23 16:20:48 -0700

    Completed part1 of explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,6 +2,45 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+var synth = window.speechSynthesis;
+
+function populateVoiceList() {
+  var voiceSelect = document.querySelector('select');
+  var voices = [];
+  voices = synth.getVoices();
+
+  for(var i = 0; i < voices.length ; i++) {
+    var option = document.createElement('option');
+    option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+
+    if(voices[i].default) {
+      option.textContent += ' -- DEFAULT';
+    }
+
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
+    voiceSelect.appendChild(option);
+  }
+}
+
+function speakPlease() {
+  var inputTxt = document.getElementsByTagName("textarea");
+  var utterThis = new SpeechSynthesisUtterance(inputTxt[0].value);
+  var faceImage = document.querySelectorAll('img')[0];
+
+  faceImage.setAttribute("src", "assets/images/smiling-open.png");
+  synth.speak(utterThis);
+
+  utterThis.addEventListener('end', changePic);
+}
+
+function changePic() {
+  var faceImage = document.querySelectorAll('img')[0];
+  faceImage.setAttribute("src", "assets/images/smiling.png");
+}
+
 function init() {
-  // TODO
+  populateVoiceList();
+  var talkButton = document.getElementsByTagName("button");
+  talkButton[0].addEventListener('click', speakPlease);
 }
